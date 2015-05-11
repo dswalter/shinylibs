0b83bb9cf09f8ed3e9e08c605383c3134af4cbcc
dswalter <dswalter@gmail.com>
2015-05-11 09:23:53 -0400

    update app to actually render plots

--- errortypes/server.R
+++ errortypes/server.R
@@ -1,4 +1,45 @@
 library(shiny)
+library(ggplot2)
+library(grid)
+
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
+
+
+
+
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
@@ -17,4 +58,61 @@ shinyServer(function(input, output) {
     # draw the histogram with the specified number of bins
     hist(x, breaks = bins, col = 'darkgray', border = 'white')
   })
+  
+  
+
+  
+  output$nullplot1 <-renderPlot({
+      xmin<-10
+      xmax<-20
+      mean<-15
+      sd<-1
+      decision_point<-input$decision_point
+      rng<-seq(xmin,xmax,0.05)
+      null_vals<-dnorm(x=rng,mean=mean,sd=sd)
+      
+      #null_vals<-dnorm(x=rng,mean=input$onemean,sd=input$onesdev)
+      nulldf<-data.frame(x=rng,null_vals)
+      nulldf$type1vals<-nulldf$null_vals
+      nulldf$type1vals[nulldf$x>decision_point]<-0
+      type_1_df<-subset(nulldf,x<=decision_point)
+      
+      nullplot<-ggplot(data=nulldf,aes(x=x))+xlim(xmin,xmax)+
+        ylab("Density")+xlab("Possible Lengths")+
+        ggtitle("Null Hypothesis Distribution")+
+        geom_ribbon(data=nulldf,aes(ymin=0,ymax=null_vals),fill="steelblue")+
+        geom_vline(x=decision_point)+
+        geom_ribbon(data=type_1_df, aes(ymin=0,
+                                        ymax=type1vals),fill="red")
+      nullplot
+  })
+  
+  
+  output$altplot1 <-renderPlot({
+    xmin<-10
+    xmax<-20
+    mean<-13
+    sd<-1
+    decision_point<-input$decision_point
+    rng<-seq(xmin,xmax,0.05)
+    null_vals<-dnorm(x=rng,mean=mean,sd=sd)
+    
+    #null_vals<-dnorm(x=rng,mean=input$onemean,sd=input$onesdev)
+    nulldf<-data.frame(x=rng,null_vals)
+    nulldf$type1vals<-nulldf$null_vals
+    nulldf$type1vals[nulldf$x>decision_point]<-0
+    type_1_df<-subset(nulldf,x<=decision_point)
+    
+    altplot<-ggplot(data=nulldf,aes(x=x))+xlim(xmin,xmax)+
+      ylab("Density")+xlab("Possible Lengths")+
+      ggtitle("Alternate Hypothesis Distribution")+
+      geom_ribbon(data=nulldf,aes(ymin=0,ymax=null_vals),fill="red")+
+      geom_vline(x=decision_point)+
+      geom_ribbon(data=type_1_df, aes(ymin=0,
+                                      ymax=type1vals),fill="steelblue")
+    altplot
+  })
+  
+  
+  
 })
--- errortypes/ui.R
+++ errortypes/ui.R
@@ -4,21 +4,36 @@ library(shiny)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Type 1 and Type 2 NOT YET HERE!"),
+  titlePanel("Type 1 and Type 2 Errors"),
   
   # Sidebar with a slider input for the number of bins
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
-    
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("distPlot")
-    )
-  )
-))
+      
+      p("When performing a hypothesis test, there are a number of possible outcomes
+        based on our null and alternative hypotheses."),
+      p("Let's say we know absolutely that the null hypothesis is true. This would, of course, 
+        make the hypothesis test completely unnecessary. But let's think this through. If $H_0$ 
+        is true, and evaluating our data, we reject the null, we have concluded incorrectly. We call this a 
+        'type 1 error.' It is also known as '$\alpha$' or 'significance level.' But if $H_0$ is 
+        true and we fail to reject the null (FTR for short), then we have made the correct decision."),
+      p("On the other hand, if we know absolutely $H_a$ is true, then if we reject the null, 
+        we have come to the correct conclusion. We call this 'power.' But if $H_a$ is true and 
+        we fail to reject the null, we have again concluded incorrectly. But this is the 
+        opposite type of mistake. We call incorrectly failing to reject the null a 'type 2 
+        error,' or '$\beta$.' Because power and $\beta$ are the only possible outcomes if the
+        H_a is true, by rules of probability, power + $\beta$ = 1."),
+      p("Let's look at an example."),
+      h4("Bolts"),
+      sliderInput("decision_point",
+                  "Choose the decision line:",
+                  value = 14,
+                  min = 12,
+                  max = 15,
+                  step = 0.05),
+      plotOutput("nullplot1"),
+      plotOutput("altplot1"),
+      p("More text should go here")
+      )
+      )
+      )
